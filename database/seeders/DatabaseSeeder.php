8f957cf281f1c3ae61c8bc922b59f228f5f5e340
Winnie <winnie131212592@gmail.com>
2024-09-25 13:20:28 +0800

    fix user pass seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password')
         ]);
     }
 }
