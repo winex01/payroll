e5df771d950e62e21996235e604bf08f6e84ee85
Winnie <winnie131212592@gmail.com>
2024-10-24 13:20:43 +0800

    pay basis, migration, model, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,5 +34,6 @@ class DatabaseSeeder extends Seeder
         $this->call(JobStatusesTableSeeder::class);
         $this->call(EmploymentStatusesTableSeeder::class);
         $this->call(EmploymentDetailTypesTableSeeder::class);
+        $this->call(PayBasesTableSeeder::class);
     }
 }
---
+++ database/seeders/PayBasesTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PayBasesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('pay_bases')->delete();
+        
+        \DB::table('pay_bases')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Daily Paid',
+                'created_at' => '2024-10-24 13:17:50',
+                'updated_at' => '2024-10-24 13:17:50',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Hourly Paid',
+                'created_at' => '2024-10-24 13:18:02',
+                'updated_at' => '2024-10-24 13:18:02',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Monthly Paid',
+                'created_at' => '2024-10-24 13:18:12',
+                'updated_at' => '2024-10-24 13:18:12',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'Pro-rated Monthly Paid',
+                'created_at' => '2024-10-24 13:18:20',
+                'updated_at' => '2024-10-24 13:18:20',
+            ),
+        ));
+        
+        
+    }
+}
